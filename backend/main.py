308a60f317345b290de979896a487108d461e4ba
F4TB0Yz <jfduarte09@gmail.com>
2026-04-12 20:43:16 -0500

    feat(db): integración con supabase y persistencia de encodings faciales
    Se añadió el cliente de Supabase para gestionar el registro de estudiantes y la validación de ingresos. Se implementó la lógica de comparación de vectores en memoria para identificación biométrica y el registro automático en la tabla de ingresos. Cubre HU01, HU02 y lógica de HU04.

--- backend/main.py
+++ backend/main.py
@@ -1,61 +1,118 @@
 from fastapi import FastAPI, File, UploadFile, status, Response
+from supabase import create_client, Client
+from dotenv import load_dotenv
 from PIL import Image
+
 import numpy as np
-
 import face_recognition
-import io
+import io, os
 
-app = FastAPI(title="Sistema de Reconocimiento Facial Universidad")
+# Configuracion Supabase ------------------------------------------------  
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+# ------------------------------------------------
+# Funciones Auxiliares ---------------------------
+def extract_vector(content_bytes: bytes):
+    # 1. Cargamos los bytes crudos a una imagen de Pillow
+    image_pil = Image.open(io.BytesIO(content_bytes))
+
+    # 2. Forzamos la imagen a RGB. Si es PNG con fondo transparente
+    # lo vuelve negro/blanco
+    image_rgb = image_pil.convert("RGB")
+
+    # 3. face_recognition necesita un arreglo matematico (lista), no un objeto de imagen
+    image_list = np.array(image_rgb)
+
+    # Aquí ocurre la extracción de características
+    encodings = face_recognition.face_encodings(image_list)
+
+    if len(encodings) == 0:
+        raise ValueError("No se detectaron rostros")
+    if len(encodings) > 1:
+        raise ValueError("Se detectaron varios rostros")
+    
+    return encodings[0].tolist()
+
+
+app = FastAPI(title="Sistema de Reconocimiento Facial Universidad") 
 
 @app.post("/reconocer")
-async def reconocer_rostro(response: Response, file: UploadFile = File(...)):
-    # Leer la imagen que mandan en memoria
-    content = await file.read()
-
+async def recognize_face(response: Response, file: UploadFile = File(...)):
     try:
-        # 1. Cargamos los bytes crudos a una imagen de Pillow
-        image_pil = Image.open(io.BytesIO(content))
+        # Leer la imagen que mandan en memoria
+        content: bytes = await file.read()
+        new_vector: list[float] = extract_vector(content)
 
-        # 2. Forzamos la imagen a RGB. Si es PNG con fondo transparente
-        # lo vuelve negro/blanco
-        image_rgb = image_pil.convert("RGB")
-
-        # 3. face_recognition necesita un arreglo matematico (lista), no un objeto de imagen
-        image_list = np.array(image_rgb)
-
-        # Aquí ocurre la extracción de características
-        encodings = face_recognition.face_encodings(image_list)
-
-        if len(encodings) == 0:
-            response.status_code = status.HTTP_400_BAD_REQUEST
-            return {
-                "status": "error",
-                "status_code": 400,
-                "mensaje": "No se detectaron rostros"
-            }
+        # 1. Extraer a todos los estudiantes
+        db_response = supabase.table("Estudiantes").select("*").execute()
+        students_db: list[dict] = db_response.data
         
-        if len(encodings) > 1:
-            response.status_code = status.HTTP_400_BAD_REQUEST
+        if not students_db:
+            response.status_code = status.HTTP_404_NOT_FOUND
             return {
                 "status": "error",
-                "status_code": 400,
-                "mensaje": "Se detectaron varios rostros"
+                "mensaje": "Estudiante no encontrado"
             }
 
-        # Lo pasamos a lista normal porque JSON no entiende arreglos de Numpy
-        encoding_rostro = encodings[0].tolist()
+        # 2. Comparar con cada estudiante
+        for student in students_db:
+            database_vector = np.array(student["vector_rostro"])
 
+            # tolerance=0.5 es el nivel de exigencia. Mas bajo = mas estricto (Por defecto es 0.6)
+            is_match: bool = face_recognition.compare_faces([database_vector], np.array(new_vector), tolerance=0.5)[0]
+            
+            if is_match:
+                # 3. Registramos el ingreso
+                supabase.table("Ingresos").insert({
+                    "estudiante_id": student["id"],
+                }).execute()
+
+                response.status_code = status.HTTP_200_OK
+                return {
+                    "status": "success",
+                    "mensaje": f"Acceso concedido. Bienvenido {student['nombres']} {student['apellidos']}",
+                    "estudiante_id": student["id"],
+                    "estudiante": f"{student['nombres']} {student['apellidos']}",
+                }
+            
         return {
-            "status": "success",
-            "status_code": 200,
-            "mensaje": "Rostro detectado correctamente",
-            "vector_length": len(encoding_rostro),
-            "vector": encoding_rostro
+            "status": "warning",
+            "mensaje": "Rostro no reconocido! Intruso detectado",
         }
     except Exception as e:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {
             "status": "fatal",
-            "status_code": 500,
+            "mensaje": f"Se rompió procesando la imagen: {str(e)}"
+        }
+
+# Registrar Estudiante (Iteración 2)
+@app.post("/estudiantes")
+async def register_student(response: Response, first_names: str, last_names: str, file: UploadFile = File(...)):
+    try:  
+        content = await file.read()
+        vector = extract_vector(content)
+
+        # Guardar en supabase
+        new_student = {
+            "nombres": first_names,
+            "apellidos": last_names,
+            "vector_rostro": vector
+        }
+        supabase.table("Estudiantes").insert(new_student).execute()
+
+        response.status_code = status.HTTP_201_CREATED
+        return {
+            "status": "success",
+            "mensaje": "Estudiante registrado correctamente"
+        }
+    except Exception as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {
+            "status": "fatal",
             "mensaje": f"Se rompió procesando la imagen: {str(e)}"
         }
